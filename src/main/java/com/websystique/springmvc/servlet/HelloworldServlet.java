f206fea0c6b58f4f98efbf68b9b665d4b58489aa
amarnath0504 <amarnath504@gmail.com>
2018-03-21 15:27:51 +0530

    first commit

---
+++ src/main/java/com/websystique/springmvc/servlet/HelloworldServlet.java
@@ -0,0 +1,28 @@
+package com.websystique.springmvc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloworldServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		resp.getWriter().write("hello this is from spring woth hibernate spring security");
+	}
+	
+	
+
+}
